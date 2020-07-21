3665900c3eddf4c608715a528b76c762fdc6448e
xsadia <lipearthur@hotmail.com>
2020-07-21 03:00:06 -0300

    codigo arrumado

--- class/usuarios.php
+++ class/usuarios.php
@@ -53,6 +53,41 @@ class Usuarios {
             $this->setDtCadastro(new DateTime($row['dtcadastro']));
         }
     }
+
+
+    public static function search($login){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    public static function getList(){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+    public function login($login, $password){
+       
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ':LOGIN'=>$login,
+            ':PASSWORD'=>$password
+        ));
+
+        if(count($results) > 0){
+            $row = $results[0];
+            $this->setIdUsuario($row['idusuario']);
+            $this->setDesLogin($row['deslogin']);
+            $this->setDesSenha($row['dessenha']);
+            $this->setDtCadastro(new DateTime($row['dtcadastro']));
+        }
+        else{
+            throw new Exception("Senha ou Login invalidos");
+            
+        }
+    }
     
     public function __toString(){
         return json_encode(array(
--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 
 require_once("config.php");
 
-$user = new Usuarios;
+//$user = new Usuarios;
+/*
+$user->loadById(4);
+echo "<br>";*/
 
-$user->loadById(3);
+/*$list = Usuarios::getList();
 
-echo $user;
+echo json_encode($list);*/
+
+//$search = Usuarios::search("fe");
+
+//echo json_encode($search);
+
+$usuario = new Usuarios();
+
+$usuario->login("fefe", "1234");
+
+echo $usuario;
